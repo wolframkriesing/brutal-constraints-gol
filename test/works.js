e97220b901d354cfa9c6b528e618b8ae78f08cd3
Wolfram Kriesing <wk@uxebu.com>
2015-09-18 15:08:01 +0200

    Add some todos for where to start in the next session.

--- test/works.js
+++ test/works.js
@@ -39,7 +39,7 @@ describe('universe', function() {
     it('of three diagonal cells contains middle cell', function() {
       let universe = {
         positions: [
-          { x:1, y:1 },
+          { x:1, y:1 }, // TODO the position is duplicated many times, extract
           { x:0, y:0 },
           { x:2, y:2 }
         ]
@@ -60,6 +60,9 @@ describe('universe', function() {
         ]
       };
 
+      // TODO we need to determine 1 from the universe somehow. this is the
+      // rule for survival. then we can extract 49 and 65 into new tick method
+      // and combine with existing one. maybe.
       let nextGeneration = newUniverseWithOneCellFrom(universe, 1);
       
       assert.deepEqual(nextGeneration, {positions: [{x: 1, y: 1}]});
