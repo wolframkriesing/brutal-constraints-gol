c98c1df498e05e13c582dc9950b6f8c97501623e
Wolfram Kriesing <wk@uxebu.com>
2015-09-18 13:44:29 +0200

    Rename test steps.

--- test/works.js
+++ test/works.js
@@ -9,38 +9,40 @@ describe('test setup works?', function() {
 function tick() {
   return {};
 }
-describe('game of life', function() {
-  it('an empty universe is empty in the next generation', function() {
-    let universe = {
+describe('universe', function() {
+  describe('next generation', function() {
+    it('of empty universe is empty', function() {
+      let universe = {
+        
+      };
       
-    };
-    
-    let nextGeneration = tick();
-    
-    assert.deepEqual(nextGeneration, {});
-  });
-
-  it('a universe of one cell is empty in next generation', function() {
-    let universe = {
-      oneCell: {}
-    };
-
-    let nextGeneration = tick();
-    
-    assert.deepEqual(nextGeneration, {});
-  });
-
-  it('a universe with three diagonal cells lets one survive in the next generation', function() {
-    let universe = {
-      oneCell: { x:1, y:1 },
-      oneCell2: { x:0, y:0 },
-      third: { x:2, y:2}
-    };
-
-    let nextGeneration = {oneCell: {x: 1, y: 1}};
-    
-    assert.deepEqual(nextGeneration, {
-      oneCell: {x: 1, y: 1}
+      let nextGeneration = tick();
+      
+      assert.deepEqual(nextGeneration, {});
+    });
+  
+    it('of one cell is empty', function() {
+      let universe = {
+        oneCell: {}
+      };
+  
+      let nextGeneration = tick();
+      
+      assert.deepEqual(nextGeneration, {});
+    });
+  
+    it('of three diagonal cells contains middle cell', function() {
+      let universe = {
+        oneCell: { x:1, y:1 },
+        oneCell2: { x:0, y:0 },
+        third: { x:2, y:2}
+      };
+  
+      let nextGeneration = {oneCell: {x: 1, y: 1}};
+      
+      assert.deepEqual(nextGeneration, {
+        oneCell: {x: 1, y: 1}
+      });
     });
   });
 });
